cf832942a800ccdf6dbef58d430d2fcedbdad851
Torycd <vicidundun@gmail.com>
2025-09-13 22:36:51 +0100

    commit

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -3,6 +3,9 @@ import styles from "./City.module.css";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useCities } from "../context/CitiesContext";
+import Spinner from "./Spinner";
+import Button from "./Button";
+import BackButton from "./BackButton";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -14,13 +17,14 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  const { getCity, currentCity } = useCities();
+  const { getCity, currentCity, isLoading } = useCities();
 
   useEffect(() => {
     getCity(id);
   }, [id]);
 
   const { cityName, emoji, date, notes } = currentCity;
+  if (isLoading) return <Spinner />;
   return (
     <div className={styles.city}>
       <div className={styles.row}>
@@ -54,7 +58,7 @@ function City() {
       </div>
 
       <div>
-        {/* <ButtonBack /> */}
+        <BackButton />
       </div>
     </div>
   );
